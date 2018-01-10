de3ac3edc6376687aabd7647ee7f591183b6a6b1
Steven <dilbersteven@gmail.com>
2018-01-10 15:02:01 -0500

    Our Rates -added Table, minor editing

--- src/components/Legal.js
+++ src/components/Legal.js
@@ -6,7 +6,7 @@ class Legal extends Component {
       <div className="content legal">
         <h1>Legal</h1>
         <br/>
-        <p>We are insured and bonded through Pet Sitter's Associates</p>
+        <p>We are insured and bonded through the Pet Sitter's Associates</p>
       </div>
     );
   }
