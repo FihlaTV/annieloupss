c231e6b3f7781c68bf1f45b3cdbf22955c21c4d7
Jon.Prell <JTP709@gmail.com>
2018-01-10 15:00:43 -0500

    added stock photos to customer's page

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -20,72 +20,72 @@ class Customers extends Component {
     this.closeModal = this.closeModal.bind(this);
     this.state = {
       showModal: false,
-      modalImg: null,
+      modalImg: 'pup1-lg.jpeg',
       carousel: {
         carousel_1: {
-          picture:'http://via.placeholder.com/900x300',
-          label:'First slide label',
-          text:'Nulla vitae elit libero, a pharetra augue mollis interdum.'
+          picture:'pup-banner-1.jpeg',
+          label:'Tug of War!',
+          text:'All visits include playtime!'
         },
         carousel_2: {
-          picture:'http://via.placeholder.com/900x300',
-          label:'Second slide label',
-          text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          picture:'pup-banner-2.jpeg',
+          label:'Out for a stroll...',
+          text:'We love walking as much as they do!',
         },
         carousel_3: {
-          picture:'http://via.placeholder.com/900x300',
-          label:'Third slide label',
-          text:'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
+          picture:'pup-banner-3.jpeg',
+          label:'Ready for my close-up',
+          text:'Regardless if they\'re a playful pup or a lazy dog, we love all personalities!'
         }
       },
       individualImages :{
         image_1:{
-          thumbnail:'http://via.placeholder.com/171x180',
-          fullsize:'http://via.placeholder.com/1710x1800'
+          thumbnail:'pup1-sm.jpeg',
+          fullsize:'pup1-lg.jpeg'
         },
         image_2:{
-          thumbnail:'http://via.placeholder.com/171x180',
-          fullsize:'http://via.placeholder.com/1710x1800'
+          thumbnail:'pup2-sm.jpeg',
+          fullsize:'pup2-lg.jpeg'
         },
         image_3:{
-          thumbnail:'http://via.placeholder.com/171x180',
-          fullsize:'http://via.placeholder.com/1710x1800'
+          thumbnail:'pup3-sm.jpg',
+          fullsize:'pup3-lg.jpg'
         },
         image_4:{
-          thumbnail:'http://via.placeholder.com/171x180',
-          fullsize:'http://via.placeholder.com/1710x1800'
+          thumbnail:'pup4-sm.jpeg',
+          fullsize:'pup4-lg.jpeg'
         },
         image_5:{
-          thumbnail:'http://via.placeholder.com/171x180',
-          fullsize:'http://via.placeholder.com/1710x1800'
+          thumbnail:'pup5-sm.jpeg',
+          fullsize:'pup5-lg.jpeg'
         },
         image_6:{
-          thumbnail:'http://via.placeholder.com/171x180',
-          fullsize:'http://via.placeholder.com/1710x1800'
+          thumbnail:'pup6-sm.jpeg',
+          fullsize:'pup6-lg.jpeg'
         },
         image_7:{
-          thumbnail:'http://via.placeholder.com/171x180',
-          fullsize:'http://via.placeholder.com/1710x1800'
+          thumbnail:'pup7-sm.jpeg',
+          fullsize:'pup7-lg.jpeg'
         },
         image_8:{
-          thumbnail:'http://via.placeholder.com/171x180',
-          fullsize:'http://via.placeholder.com/1710x1800'
+          thumbnail:'pup8-sm.jpeg',
+          fullsize:'pup8-lg.jpeg'
         },
         image_9:{
-          thumbnail:'http://via.placeholder.com/171x180',
-          fullsize:'http://via.placeholder.com/1710x1800'
+          thumbnail:'pup9-sm.jpeg',
+          fullsize:'pup9-lg.jpeg'
         },
         image_10:{
-          thumbnail:'http://via.placeholder.com/171x180',
-          fullsize:'http://via.placeholder.com/1710x1800'
+          thumbnail:'pup10-sm.jpeg',
+          fullsize:'pup10-lg.jpeg'
         },
         image_11:{
-          thumbnail:'http://via.placeholder.com/171x180',
-          fullsize:'http://via.placeholder.com/1710x1800'
+          thumbnail:'pup11-sm.jpeg',
+          fullsize:'pup11-lg.jpeg'
         },
         image_12:{
-          thumbnail:'http://via.placeholder.com/171x180',
-          fullsize:'http://via.placeholder.com/1710x1800'
+          thumbnail:'pup12-sm.jpeg',
+          fullsize:'pup12-lg.jpeg'
         }
       }
     }
@@ -106,9 +106,10 @@ class Customers extends Component {
 
   renderCarousel(key) {
     const data = this.state.carousel[key];
+    const carouselImg = require(`../img/${data.picture}`);
     const carouselInstance = (
       <Carousel.Item key={key}>
-        <img width={900} height={300} alt="900x300" src={data.picture} />
+        <img width={900} height={300} alt="900x300" src={carouselImg} />
         <Carousel.Caption>
           <h3>{data.label}</h3>
           <p>{data.text}</p>
@@ -120,9 +121,10 @@ class Customers extends Component {
 
   renderThumbnails(key){
     const data = this.state.individualImages[key];
+    const thumbImg = require(`../img/${data.thumbnail}`);
     const imageInstance = (
       <Col xs={6} md={3} key={key}>
-        <Thumbnail href="#" alt="171x180" src={data.thumbnail} onClick={() => { this.openModal(data.fullsize) }} />
+        <Thumbnail href="#" alt="171x180" src={thumbImg} onClick={() => { this.openModal(data.fullsize) }} />
       </Col>
     );
     return imageInstance
@@ -134,11 +136,15 @@ class Customers extends Component {
         very popover. such engagement
       </Popover>
     );
+
     const tooltip = (
       <Tooltip id="modal-tooltip">
         wow.
       </Tooltip>
     );
+
+    const modalImg = require(`../img/${this.state.modalImg}`);
+
     return (
       <Grid fluid={true} className="content customers">
         <div>
@@ -161,7 +167,7 @@ class Customers extends Component {
             <Modal.Title>Another Happy Customer!</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <Image alt="Happy Customer" src={this.state.modalImg} responsive />
+            <Image alt="Happy Customer" src={modalImg} responsive />
           </Modal.Body>
         </Modal>
       </Grid>
